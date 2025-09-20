9d4bff6180a833dce99dc7af88a98a3dff9a93e2
adnanrizvi-turing <adnan.rizvi@turing.com>
2025-09-20 15:45:59 +0530

    first initial

---
+++ data/data.ts
@@ -0,0 +1,24 @@
+const currentYear = new Date().getFullYear();
+const data = {
+    sitename: "Bartrix",
+    sitetagline: "Something great is on the way! ✨ 🔥",
+    siteurl: "/",
+    sitelogo: "",
+    title: "Coming Soon!",
+    description: "We&apos;re currently working 👨‍💻 hard to bring you something great, and we can&apos;t wait to share it with you 📅. Our team is putting the finishing touches on a new project 🚀 that we think you&apos;ll love 😍.",
+    newsletterheading: "Stay tuned for our live updates!",
+    copyrightText: `Copyright © ${currentYear}`,
+    socialIconsHeading: "Follow Us 📣",
+    hideSubscribeForm: false, // make true to disable subscription form 
+    socialIcons: [
+      
+    ],
+    hide :{
+        subscribeForm: false, // make true to disable subscription form         
+        header: false,
+        content: false,
+        footer: false,
+    }
+}
+
+export default data;
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ThemeSwitch from '../ThemeSwitch/ThemeSwitch'
+
+function Navigation() {
+  return (
+    <>
+    <nav className="flex sm:justify-center space-x-4">
+        {[
+            
+        ].map(([title, url]) => (
+            <a key={title} href={url} className="rounded-lg px-3 py-2 font-semibold text-slate-800 hover:bg-slate-700 hover:text-slate-100 dark:text-slate-100 dark:hover:text-slate-800 dark:hover:bg-zinc-300">{title}</a>
+        ))
+        
+        }
+           <img
+  src="/transparent.png"
+  alt="Logo"
+  className="w-16 h-16 object-contain"
+/>
+        <ThemeSwitch />
+    </nav>
+    </>
+  )
+}
+
+export default Navigation
